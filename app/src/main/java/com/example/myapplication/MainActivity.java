38847be68afd7fbbc706c10fc2dca422cf8fd64d
jyothisbenny <jyothisbenny75@gmail.com>
2021-10-08 17:57:03 +0530

    added an input text for getting the name from the user and printed a message when user clicks on the button

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick (View view){
+        TextView helloTxt = findViewById(R.id.txtHello);
+        EditText editText = findViewById(R.id.editTextName);
+
+        helloTxt.setText("Hey, "+ editText.getText().toString());
     }
 }
